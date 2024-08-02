9bb6417396a99754af85def967c849da9fb5195b
Matthew Gerring <matthew.gerring@jax.org>
2024-08-02 15:04:40 -0400

    Add a GFF reader in order to slurp up reg features.

--- src/main/java/org/geneweaver/io/reader/ReaderFactory.java
+++ src/main/java/org/geneweaver/io/reader/ReaderFactory.java
@@ -50,6 +50,7 @@ public class ReaderFactory {
 		tmp.put("gvf", 			VariantReader.class);
 		tmp.put("vcf", 			FastVCFReader.class);
 		tmp.put("bed", 			BedReader.class);
+		tmp.put("gff", 			RegulatoryFeatureReader.class);
 		tmp.put("step", 		StepReader.class);
 		
 		// If there are multiple xls formats, we will have to ask 
