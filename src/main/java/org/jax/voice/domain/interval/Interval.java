feaecc0eace86e1da5e8cf9329bc71cace4188e3
Matthew Gerring <matthew.gerring@jax.org>
2026-02-10 15:54:19 -0500

    Made Serializable

--- src/main/java/org/jax/voice/domain/interval/Interval.java
+++ src/main/java/org/jax/voice/domain/interval/Interval.java
@@ -1,5 +1,13 @@
 package org.jax.voice.domain.interval;
 
+import java.io.Serializable;
 import java.util.Map;
 
-public record Interval(int start, int end, Object id, String chr, Map<String, Object> meta) {}
+public record Interval(int start, 
+		int end, 
+		Object id, 
+		String chr, 
+		Map<String, Object> meta) implements Serializable {
+	
+	
+}
