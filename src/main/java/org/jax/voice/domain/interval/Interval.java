9981e8d487fd75a275787e3a4b5be2ab2d97aef3
Matthew Gerring <matthew.gerring@jax.org>
2026-02-10 15:42:26 -0500

    Removed interface.

--- src/main/java/org/jax/voice/domain/interval/Interval.java
+++ src/main/java/org/jax/voice/domain/interval/Interval.java
@@ -2,4 +2,4 @@ package org.jax.voice.domain.interval;
 
 import java.util.Map;
 
-public record Interval(int start, int end, Object id, String chr, Map<String, Object> meta) implements IInterval{}
+public record Interval(int start, int end, Object id, String chr, Map<String, Object> meta) {}
